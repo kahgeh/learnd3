bd1356ac2758fae5dea3c5596a1aa14b651e939a
kahgeh.tan <kahgeh.tan@xero.com>
2019-10-29 06:59:07 +1100

    add annulus chart, split spa to routes and add mainlayout

---
+++ MainLayout.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import './MainLayout.css';
+import { useHistory, useLocation } from "react-router";
+import LineBarChart from "./LineBarChart";
+import AnnulusChart from "./AnnulusChart";
+
+export enum ChartTypeNames {
+    LineAndBar = "lineBarChart",
+    Annulus = "annulusChart"
+}
+export const ChartTypes = [{
+    name: ChartTypeNames.LineAndBar,
+    title: 'Line and Bar Chart',
+    component: LineBarChart
+}, {
+    name: ChartTypeNames.Annulus,
+    title: 'Annulus Chart',
+    component: AnnulusChart
+}]
+
+const MainLayout: React.FunctionComponent = ({ children }) => {
+    const history = useHistory();
+    const location = useLocation();
+    console.log(location);
+    let selectedChartTitle = ChartTypes[0].title;
+    let selectedChartTypes = ChartTypes.filter((chartType) => location.pathname.endsWith(chartType.name));
+    if (selectedChartTypes && selectedChartTypes.length > 0) {
+        selectedChartTitle = selectedChartTypes[0].title;
+    }
+    return (<>
+        <div className='drawer-container'>
+            {
+                ChartTypes.map((chartType, i) => (<div key={i} className="drawer-item">
+                    <div className="button" onClick={() => history.push(chartType.name)}>{chartType.title}</div>
+                </div>))
+            }
+        </div>
+        <div className="content-container">
+            <div className="chart-title">
+                {selectedChartTitle}
+            </div>
+            <div className="chart-container">
+                {children}
+            </div>
+            <div className="chart-footer">
+            </div>
+        </div>
+    </>);
+}
+
+export default MainLayout;
--- app.tsx
+++ app.tsx
@@ -1,43 +1,16 @@
 import * as React from 'react';
 import { render } from 'react-dom'
-import './app.css';
-import LineBarChart from './LineBarChart';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { ChartTypes } from './MainLayout';
 
-enum ChartTypes {
-    LineAndBar = "Line and bar chart",
-    Annulus = "Annulus"
-}
 
-const getChart = (type: ChartTypes) => {
-    if (type === ChartTypes.LineAndBar) {
-        return (<LineBarChart />)
-    }
-}
 const App: React.FunctionComponent = () => {
-    const [selectedChart, setState] = React.useState(ChartTypes.LineAndBar)
-
-    return (<>
-        <div className='drawer-container'>
-            <div className="drawer-item">
-                <div className="button">{selectedChart}</div>
-            </div>
-            <div className="drawer-item">
-                <div className="button">Annulus</div>
-            </div>
-        </div>
-        <div className="content-container">
-            <div className="chart-title">
-                {selectedChart}
-            </div>
-            <div className="chart-container">
-                {
-                    getChart(selectedChart)
-                }
-            </div>
-            <div className="chart-footer">
-            </div>
-        </div>
-    </>);
+    return (
+        <BrowserRouter>
+            <Route exact path="/" component={ChartTypes[0].component} />
+            {ChartTypes.map((chartType, i) => (<Route key={i} exact path={`/${chartType.name}`} component={chartType.component} />))}
+        </BrowserRouter>);
 }
 
 render(<App />, document.getElementById('root'))
